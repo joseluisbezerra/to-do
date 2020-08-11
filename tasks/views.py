37fbf6cbe725258025eecaa4f06d3e2d755a1eac
Luis Bezerra <luisbezerra.work@gmail.com>
2020-08-11 14:31:29 -0300

    Adicionado a ForeignKey entre User e Task

--- tasks/views.py
+++ tasks/views.py
@@ -12,9 +12,9 @@ def task_list(request):
     search = request.GET.get('search')
 
     if (search):
-        tasks = Task.objects.filter(title__icontains=search)
+        tasks = Task.objects.filter(title__icontains=search, user=request.user)
     else:
-        tasks_list = Task.objects.all().order_by('-created_at')
+        tasks_list = Task.objects.all().order_by('-created_at').filter(user=request.user)
         paginator = Paginator(tasks_list, 3)
         page = request.GET.get('page')
         tasks = paginator.get_page(page)
@@ -36,6 +36,7 @@ def new_task(request):
         if (form.is_valid()):
             task = form.save(commit=False)
             task.done = 'doing'
+            task.user = request.user
             task.save()
             messages.success(request, 'Tarefa adicionada com sucesso')
             return redirect('/')
