f56fb402a0a042ad1e2db3f8577dfa7785c78543
Luis Bezerra <luisbezerra.work@gmail.com>
2020-08-10 22:59:29 -0300

    Adicionado o CRUD da aplicação junto com a função de messages

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task-list'),
+    path('tasks/<int:id>', views.task_view, name='task-view'),
+    path('newtask/', views.new_task, name='new-task'),
+    path('edit/<int:id>', views.edit_task, name='edit-task'),
+    path('delete/<int:id>', views.delete_task, name='delete-task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 
+from .models import Task
+from .forms import TaskForm
 
 def task_list(request):
-    return render(request, 'tasks/list.html')
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+def task_view(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
+
+def new_task(request):
+    if (request.method == 'POST'):
+        form = TaskForm(request.POST)
+
+        if (form.is_valid()):
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            messages.success(request, 'Tarefa adicionada com sucesso')
+            return redirect('/')
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/addtask.html', {'form': form})
+
+def edit_task(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+
+    if (request.method == 'POST'):
+        form = TaskForm(request.POST, instance=task)
+
+        if (form.is_valid()):
+            task.save()
+            messages.success(request, 'Tarefa editada com sucesso')
+            return redirect('/')
+        else:
+            return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+    else:
+        return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
+def delete_task(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+
+    messages.success(request, 'Tarefa deletada com sucesso')
+
+    return redirect('/')
