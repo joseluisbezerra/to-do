22dbbde04e5cb9cfacb5043c76bf06e16a9fb8d7
Luis Bezerra <luisbezerra.work@gmail.com>
2020-08-11 14:56:26 -0300

    Adicionada a funcionalidade de concluir tarefa

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('tasks/<int:id>', views.task_view, name='task-view'),
     path('newtask/', views.new_task, name='new-task'),
     path('edit/<int:id>', views.edit_task, name='edit-task'),
+    path('chagestatus/<int:id>', views.change_status, name='change-status'),
     path('delete/<int:id>', views.delete_task, name='delete-task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -71,3 +71,16 @@ def delete_task(request, id):
     messages.success(request, 'Tarefa deletada com sucesso')
 
     return redirect('/')
+
+@login_required
+def change_status(request, id):
+    task = get_object_or_404(Task, pk=id)
+    
+    if (task.done == 'doing'):
+        task.done = 'done'
+    else:
+        task.done = 'doing'
+
+    task.save()
+
+    return redirect('/')
